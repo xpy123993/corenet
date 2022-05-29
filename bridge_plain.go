c9258d2d79df5914b7af1d8dedf36138fcc0b3d7
xpy123993 <xpy123993@gmail.com>
2022-05-29 16:19:32 -0700

    Change to lazy evaluate connection state.

--- bridge_plain.go
+++ bridge_plain.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"net"
 	"sync"
-	"time"
 )
 
 type clientListenerSession struct {
@@ -153,21 +152,12 @@ func (p *listenerBasedRelayProtocol) InitChannelSession(Channel string, Listener
 			return nil, err
 		}
 		return sessionInfo, nil
-	}, isClosed: false, done: make(chan struct{}), closer: ListenerConn.Close}
-	go func() {
-		ticker := time.NewTicker(30 * time.Second)
-		defer ticker.Stop()
-		select {
-		case <-session.done:
-			return
-		case <-ticker.C:
-			if _, err := ListenerConn.Write([]byte{Nop}); err != nil {
-				ListenerConn.Close()
-				session.Close()
-				return
-			}
+	}, isClosed: false, done: make(chan struct{}), closer: ListenerConn.Close, isDialerClosed: func() bool {
+		if _, err := ListenerConn.Write([]byte{Nop}); err != nil {
+			return true
 		}
-	}()
+		return false
+	}}
 	return &session, nil
 }
 
